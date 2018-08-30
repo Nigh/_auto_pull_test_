2c92bc439d4ebd080695e259425ffa10598e7b6f
Nigh <jiyucheng007@gmail.com>
2018-08-31 02:16:15 +0800

    4

--- main.lua
+++ main.lua
@@ -1,10 +1,11 @@
 math.randomseed(os.time())
+io.stdout:setvbuf('no')
 local socket = require("socket")
 i = 0
 print("version:0004")
 while true do
     i=i+1
     socket.sleep(1)
-    print(i .. ":" .. math.random() .. "\n")
+    print(i .. ":" .. math.random())
+	if i>2 then x.a=0 end
 end
-
